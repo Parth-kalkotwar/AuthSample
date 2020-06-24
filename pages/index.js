ba9c7115ad05127424499849900230f5fc876b72
Parth <parthkalkotwar@gmail.com>
2020-06-24 22:46:36 +0530

    Second COmmit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
 
-export default function Home({ res }) {
-  return <>{JSON.stringify(res)}</>;
+export default function Index({ res }) {
+  return (
+    <p>
+      To test the CORS route, open the console in a new tab on a different
+      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
+      a different method from those mentioned will be blocked by CORS
+      {JSON.stringify(res)}
+    </p>
+  );
 }
 
 export async function getStaticProps(context) {
-  const resp = await fetch("http://localhost:3000/api/login");
+  const resp = await fetch("https://auth-sample.vercel.app/api/login");
   const res = await resp.json();
   return {
     props: { res },
