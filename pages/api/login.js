5a33ba02beee1b92330629d1d9514a55c0ec4c57
Parth <parthkalkotwar@gmail.com>
2020-06-25 12:57:16 +0530

    Commit

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,11 @@
 import Cors from "cors";
 import initMiddleware from "../../lib/init-middleware";
+import { compare } from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+import { sign } from "jsonwebtoken";
+import { pool } from "../../database/dbConfig";
+import cookie from "cookie";
+import { secret } from "../../auth/secret";
 
 const cors = initMiddleware(
   Cors({
@@ -7,7 +13,59 @@ const cors = initMiddleware(
   })
 );
 
-export default async function handler(req, res) {
+export default async function login(
+  req = NextApiRequest,
+  res = NextApiResponse
+) {
   await cors(req, res);
-  res.json({ message: "Hello Everyone!" });
+  if (typeof req.body === "string") {
+    req.body = JSON.parse(req.body);
+  }
+  if (req.method === "POST") {
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`,
+      [req.body.email],
+      (err, results) => {
+        if (err) {
+          res.json(err);
+        }
+        //console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          const user = results.rows[0];
+
+          compare(req.body.password, user.password, (err, isMatch) => {
+            if (err) {
+              console.log(err);
+            }
+            if (isMatch) {
+              var token = sign({ sub: user.email, name: user.name }, secret, {
+                expiresIn: "1h",
+              });
+              res.setHeader(
+                "Set-Cookie",
+                cookie.serialize("auth", token, {
+                  httpOnly: true,
+                  secure: process.env.NODE_ENV !== "development",
+                  sameSite: "strict",
+                  maxAge: 3600,
+                  path: "/",
+                })
+              );
+              res.json({ jwt: token });
+            } else {
+              res.json({ message: "Password is incorrect" });
+            }
+          });
+        } else {
+          res.json({
+            message: "No user with that email address",
+          });
+        }
+      }
+    );
+  } else {
+    res.json({ message: "We Only Support Post REquest" });
+  }
+  //res.json({ message: "Hello Everyone!" });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
-import Head from "next/head";
+import Link from "next/link";
 
-export default function Index({ res }) {
-  //console.log(res);
-  return <p>{JSON.stringify(res)}</p>;
+export default function Home({ res }) {
+  return (
+    <>
+      <Link href="/googlelogin">
+        <a>Google Login</a>
+      </Link>
+      <br />
+      <hr />
+      <Link href="/fblogin">
+        <a>Facebook Login</a>
+      </Link>
+      <br />
+      <hr />
+      <Link href="/register">
+        <a>Register</a>
+      </Link>
+      <br />
+      <hr />
+      <Link href="/login">
+        <a> Login</a>
+      </Link>
+      <br />
+      <hr />
+    </>
+  );
 }
-
-Index.getInitialProps = async (context) => {
-  // const resp = await fetch("http://localhost:3000/api/login");
-  const resp = await fetch("https://auth-sample.vercel.app/api/login");
+/*
+Home.getInitialProps = async (context) => {
+  const resp = await fetch("http://localhost:3000/api/login");
+  //const resp = await fetch("https://auth-sample.vercel.app/api/login");
   const res = await resp.json();
   //console.log(res);
   return { res: res };
 };
+*/
